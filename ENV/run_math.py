dfec9de3cb0e74c2c1917fbcd97568d583ad8894
Cooper N <qian_niu@brown.edu>
2025-06-08 20:40:28 -0400

    add run_mathematica functions

--- ENV/run_math.py
+++ ENV/run_math.py
@@ -1,4 +1,8 @@
 import subprocess
 import os
 
-print(os.getcwd())
+def run_math(command):
+    subprocess.run(command, shell=True)
+
+if __name__ == "__main__":
+    run_math("math -script run_math.py")
